c84a425ac3ad50a2b89f9e0b3235ee3f24e88978
Abu Sahed Arafath <arafathabusahed@gmail.com>
2026-07-18 16:47:31 +0530

    Phase 1 completed - Website structure ready

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
@@ -10,9 +11,25 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("home/index");
+});
+app.get("/about", (req, res) => {
+    res.render("about/index");
+});
+app.get("/admission", (req, res) => {
+    res.render("admission/index");
 });
 
+app.get("/apply", (req, res) => {
+    res.render("admission/apply");
+});
+
+app.post("/apply", (req, res) => {
+    console.log(req.body);
+
+    res.send("<h1>Application Submitted Successfully!</h1>");
+});
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
