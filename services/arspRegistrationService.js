008758e882b4279e1cd3be32e3d8aee2b4605a9f
Abu Sahed Arafath <arafathabusahed@gmail.com>
2026-08-09 17:04:44 +0530

    Remove emergency contact fields from ARSP registration

--- services/arspRegistrationService.js
+++ services/arspRegistrationService.js
@@ -90,17 +90,7 @@ if (existing) {
             state: data.state,
 
             pincode: data.pincode,
-
-            emergency_contact_name:
-                data.emergency_contact_name,
-
-            emergency_contact_relation:
-                data.emergency_contact_relation,
-
-            emergency_contact_mobile:
-                data.emergency_contact_mobile,
-
-            photo:
+photo:
                 req.files?.photo?.[0]?.filename || "",
 
             joining_date: data.joining_date,
