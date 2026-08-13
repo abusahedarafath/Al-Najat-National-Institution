83b5318f9f9ab41369b5bbc2922416ad324cd54c
Abu Sahed Arafath <arafathabusahed@gmail.com>
2026-08-13 07:51:39 +0530

    Fix scrolling messages admin navigation

--- server.js
+++ server.js
@@ -115,6 +115,7 @@ app.use(flash());
 
 // Global Flash Variables
 app.use((req, res, next) => {
+    res.locals.currentPath = req.path;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.info = req.flash("info");
