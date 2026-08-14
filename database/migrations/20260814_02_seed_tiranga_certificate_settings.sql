8397ca47de9e953caa25f12993650560c9f73cdf
Abu Sahed Arafath <arafathabusahed@gmail.com>
2026-08-14 23:35:38 +0530

    Seed Tiranga certificate settings

---
+++ database/migrations/20260814_02_seed_tiranga_certificate_settings.sql
@@ -0,0 +1,18 @@
+INSERT INTO tiranga_certificate_settings
+(
+    id,
+    enabled,
+    organization_name,
+    certificate_title,
+    independence_text,
+    independence_years
+)
+VALUES
+(
+    1,
+    1,
+    'ACTIVE RURAL SOCIAL PROGRESS',
+    'TIRANGA CERTIFICATE',
+    '80th INDEPENDENCE',
+    '1947 - 2027'
+);
