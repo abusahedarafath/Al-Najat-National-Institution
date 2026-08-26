616145f69cf4b7c401f7790b4e8ceb3338e04e23
Abu Sahed Arafath <arafathabusahed@gmail.com>
2026-08-26 19:08:12 +0530

    Hide RTSE application when applications are closed

--- controllers/rtseController.js
+++ controllers/rtseController.js
@@ -63,6 +63,17 @@ exports.applicationPage = async (req, res) => {
         const draft = req.session.rtseDraft || {};
         const setting = await ArspSetting.get();
 
+    const rtseSetting = await RtseSetting.get();
+
+    if (!rtseSetting || Number(rtseSetting.application_open) !== 1) {
+        return res.status(403).render(
+            "rtse/application-closed",
+            {
+                title: "RTSE Application Closed"
+            }
+        );
+    }
+
         let schools = [];
 
         try {
